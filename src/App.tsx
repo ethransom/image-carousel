210d989906068d7c8db8bbe89fcc9ea7ea1bd3bb
Ethan Ransom <derethanhausen@gmail.com>
2019-04-25 17:08:32 -0600

    Implement simple, photo-gallery style carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,178 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Images from "./Images";
+
+export default class App extends React.Component {
+  state = {
+    images: Images,
+    imageIndex: 0,
+    transitioning: false,
+    targetOpacity: 1
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.onKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.onKeyPress);
+  }
+
+  private onImageLoad = (): any => null;
+
+  private awaitImageLoad() {
+    return new Promise(resolve => {
+      this.onImageLoad = resolve;
+    });
+  }
+
+  private onKeyPress = (ev: KeyboardEvent) => {
+    if (ev.key === "ArrowLeft") {
+      this.switchImage("prev");
+    }
+    if (ev.key === "ArrowRight") {
+      this.switchImage("next");
+    }
+  };
+
+  private switchImage = async (action: "next" | "prev" | number) => {
+    if (this.state.transitioning) {
+      return;
+    }
+
+    this.setState({
+      transitioning: true,
+      targetOpacity: 0
+    });
+
+    await waitFor(300); // let CSS animate out the old image
+
+    if (action === "next") {
+      this.setState({
+        imageIndex: (this.state.imageIndex + 1) % this.state.images.length
+      });
+    }
+    if (action === "prev") {
+      let i = this.state.imageIndex - 1;
+      if (i < 0) {
+        i = this.state.images.length - 1;
+      }
+      this.setState({
+        imageIndex: i
+      });
+    }
+    if (typeof action === "number") {
+      this.setState({
+        imageIndex: action
+      });
+    }
+
+    // make sure that opacity is set in a different tick than the image source attr
+    setTimeout(
+      () =>
+        this.setState({
+          targetOpacity: 1
+        }),
+      50
+    );
+    // wait for image load
+    // await this.awaitImageLoad();
+
+    await waitFor(300); // let CSS animate in the new image
+
+    this.setState({ transitioning: false });
+  };
+
+  render() {
+    let img = this.state.images[this.state.imageIndex];
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <main>
+            <button
+              onClick={() => this.switchImage("prev")}
+              className="control"
+            >
+              <img src="/img/prev.svg" />
+            </button>
+            <img
+              key={img.src}
+              style={{
+                opacity: this.state.targetOpacity
+              }}
+              src={img.src}
+              className="carouselImg"
+              alt="logo"
+            />
+            {/* {this.state.images.map((image, i) => (
+              <img
+                key={image.src}
+                style={{
+                  display: this.state.imageIndex === i ? "inherit" : "none",
+                  opacity: this.state.targetOpacity
+                }}
+                src={image.src}
+                className="carouselImg"
+                alt="logo"
+              />
+            ))} */}
+            <button
+              onClick={() => this.switchImage("next")}
+              className="control"
+            >
+              <img src="/img/next.svg" />
+            </button>
+          </main>
+          <nav>
+            <p
+              style={{
+                display: "flex",
+                flexDirection: "row"
+                // justifyContent: "space-between"
+              }}
+            >
+              {this.state.images.map((img, i) => (
+                <img
+                  onClick={() => this.switchImage(i)}
+                  src={img.src}
+                  style={{
+                    margin: "0 1vh 0 1vh",
+                    width: "3vh",
+                    height: "3vh",
+                    objectFit: "cover",
+                    cursor: "pointer",
+                    border:
+                      "3px solid " +
+                      (i === this.state.imageIndex ? "white" : "black")
+                  }}
+                />
+              ))}
+            </p>
+            <p>
+              Credit:{" "}
+              <a className="App-link" href={img.link} target="_blank">
+                {img.credit} (flickr)
+              </a>
+            </p>
+          </nav>
+          {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+function waitFor(millis: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, millis);
+  });
+}
---
+++ src/Images.ts
@@ -0,0 +1,34 @@
+const images = [
+  {
+    src: "/img/city-hall.jpeg",
+    link:
+      "https://www.flickr.com/photos/kenlund/144446478/in/photolist-bAtvJo-89z4sf-bjxDUJ-cK4GNf-bVvECJ-9PkmJL-cK4RYu-AAezQZ-cK4JiA-bn8rG4-a4FffC-dLjUw-cK4EdG-6eefQc-cK4CJQ-2dVPUu2-cK4ENq-dLiTB-bEmc81-9fxpjs-au3Ya1-7CjwxJ-cK4QAU-4wPy9R-c4sEQw-cK4Ct7-aD5Xwr-24zC4Fn-ahBnEj-RsSB58-efy7Z3-3XFes2-3XFeJZ-bEcghv-abCJvJ-f1xQrZ-G1S1Xc-9RRpmi-dLk1R-J5nyTA-6gaXZ-cg6qhN-SDXHk5-cK4QZU-NgdEr-9fudCt-aA8QTD-c4VjuQ-2dPJi4Z-abk3hJ/",
+    credit: "Ken Lund"
+  },
+  {
+    src: "/img/downtown.jpeg",
+    link:
+      "https://www.flickr.com/photos/kenlund/144446845/in/photolist-bAtvJo-89z4sf-bjxDUJ-cK4GNf-bVvECJ-9PkmJL-cK4RYu-AAezQZ-cK4JiA-bn8rG4-a4FffC-dLjUw-cK4EdG-6eefQc-cK4CJQ-2dVPUu2-cK4ENq-dLiTB-bEmc81-9fxpjs-au3Ya1-7CjwxJ-cK4QAU-4wPy9R-c4sEQw-cK4Ct7-aD5Xwr-24zC4Fn-ahBnEj-RsSB58-efy7Z3-3XFes2-3XFeJZ-bEcghv-abCJvJ-f1xQrZ-G1S1Xc-9RRpmi-dLk1R-J5nyTA-6gaXZ-cg6qhN-SDXHk5-cK4QZU-NgdEr-9fudCt-aA8QTD-c4VjuQ-2dPJi4Z-abk3hJ/",
+    credit: "Ken Lund"
+  },
+  {
+    src: "/img/slc1.jpeg",
+    link:
+      "https://www.flickr.com/photos/kenlund/144446845/in/photolist-bAtvJo-89z4sf-bjxDUJ-cK4GNf-bVvECJ-9PkmJL-cK4RYu-AAezQZ-cK4JiA-bn8rG4-a4FffC-dLjUw-cK4EdG-6eefQc-cK4CJQ-2dVPUu2-cK4ENq-dLiTB-bEmc81-9fxpjs-au3Ya1-7CjwxJ-cK4QAU-4wPy9R-c4sEQw-cK4Ct7-aD5Xwr-24zC4Fn-ahBnEj-RsSB58-efy7Z3-3XFes2-3XFeJZ-bEcghv-abCJvJ-f1xQrZ-G1S1Xc-9RRpmi-dLk1R-J5nyTA-6gaXZ-cg6qhN-SDXHk5-cK4QZU-NgdEr-9fudCt-aA8QTD-c4VjuQ-2dPJi4Z-abk3hJ/",
+    credit: "Ken Lund"
+  },
+  {
+    src: "/img/slc2.jpeg",
+    link:
+      "https://www.flickr.com/photos/countylemonade/7707256354/in/photolist-bAtvJo-89z4sf-bjxDUJ-cK4GNf-bVvECJ-9PkmJL-cK4RYu-AAezQZ-cK4JiA-bn8rG4-a4FffC-dLjUw-cK4EdG-6eefQc-cK4CJQ-2dVPUu2-cK4ENq-dLiTB-bEmc81-9fxpjs-au3Ya1-7CjwxJ-cK4QAU-4wPy9R-c4sEQw-cK4Ct7-aD5Xwr-24zC4Fn-ahBnEj-RsSB58-efy7Z3-3XFes2-3XFeJZ-bEcghv-abCJvJ-f1xQrZ-G1S1Xc-9RRpmi-dLk1R-J5nyTA-6gaXZ-cg6qhN-SDXHk5-cK4QZU-NgdEr-9fudCt-aA8QTD-c4VjuQ-2dPJi4Z-abk3hJ/",
+    credit: "Garrett"
+  },
+  {
+    src: "/img/slc3.jpeg",
+    link:
+      "https://www.flickr.com/photos/countylemonade/6095176634/in/photolist-bAtvJo-89z4sf-bjxDUJ-cK4GNf-bVvECJ-9PkmJL-cK4RYu-AAezQZ-cK4JiA-bn8rG4-a4FffC-dLjUw-cK4EdG-6eefQc-cK4CJQ-2dVPUu2-cK4ENq-dLiTB-bEmc81-9fxpjs-au3Ya1-7CjwxJ-cK4QAU-4wPy9R-c4sEQw-cK4Ct7-aD5Xwr-24zC4Fn-ahBnEj-RsSB58-efy7Z3-3XFes2-3XFeJZ-bEcghv-abCJvJ-f1xQrZ-G1S1Xc-9RRpmi-dLk1R-J5nyTA-6gaXZ-cg6qhN-SDXHk5-cK4QZU-NgdEr-9fudCt-aA8QTD-c4VjuQ-2dPJi4Z-abk3hJ/",
+    credit: "Garrett"
+  }
+];
+
+export default images;
